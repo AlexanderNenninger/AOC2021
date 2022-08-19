bda21d64b8562779a32d227387167aedbdbdbcaf
Alexander Nenninger <Alexander.Nenninger@nttdata.com>
2022-08-19 17:28:02 +0200

    implement fusion code

--- src/day19.rs
+++ src/day19.rs
@@ -1,202 +1,466 @@
 #![allow(unused)]
+
+use std::{cell::Ref, cmp::Ordering, error::Error, str::FromStr};
+
 use itertools::Itertools;
-use num_traits::Pow;
-use std::{
-    collections::HashSet,
-    iter::FromIterator,
-    mem::size_of,
-    ops::{Add, Rem, Sub},
-};
+use na::SVector;
+extern crate nalgebra as na;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct Point<const DIM: usize> {
-    coords: [isize; DIM],
+type Point<const D: usize> = na::Point<f64, D>;
+
+fn factorial(n: usize) -> usize {
+    (1..=n).product()
 }
 
-impl<const DIM: usize> Point<DIM> {
-    fn new(coords: [isize; DIM]) -> Self {
-        Self { coords }
-    }
-}
-
-impl<const DIM: usize> Add for Point<DIM> {
-    type Output = Self;
-    fn add(self, other: Self) -> Self {
-        let mut coords = [0; DIM];
-        for i in 0..DIM {
-            coords[i] = self.coords[i] + other.coords[i];
+fn cmp_points_lexical<const D: usize>(p: &Point<D>, q: &Point<D>) -> Ordering {
+    for i in 0..D {
+        // Equal if close
+        if (p[i] - q[i]).abs() < 1e-6 {
+            continue;
         }
-        Self { coords }
+        return p[i].partial_cmp(&q[i]).unwrap();
     }
+    Ordering::Equal
 }
 
-impl<const DIM: usize> Sub for Point<DIM> {
-    type Output = Self;
-    fn sub(self, other: Self) -> Self {
-        let mut coords = [0; DIM];
-        for i in 0..DIM {
-            coords[i] = self.coords[i] - other.coords[i];
-        }
-        Self { coords }
-    }
-}
-
-impl<const DIM: usize> Pow<u32> for Point<DIM> {
-    type Output = Self;
-    fn pow(self, exp: u32) -> Self {
-        let mut coords = self.coords.clone();
-        for i in 0..DIM {
-            coords[i] = coords[i].pow(exp);
-        }
-        Self::new(coords)
-    }
-}
-
-// Used for generating consistently labled point clouds.
-impl<const DIM: usize> PartialOrd for Point<DIM> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        for i in 0..DIM {
-            if self.coords[i] != other.coords[i] {
-                return self.coords[i].partial_cmp(&other.coords[i]);
+fn reflection_matrices<const D: usize>() -> impl Iterator<Item = na::SMatrix<f64, D, D>> {
+    (0..2u32.pow(D as u32)).map(|i| {
+        let mut m = na::SMatrix::<f64, D, D>::identity();
+        for j in 0..D {
+            if i & (1 << j) != 0 {
+                m[(j, j)] = -1.0;
             }
         }
-        Some(std::cmp::Ordering::Equal)
+        m
+    })
+}
+
+fn permutation_matrices<const D: usize>() -> impl Iterator<Item = na::SMatrix<f64, D, D>> {
+    let base = [0.; D];
+    let mut working_set = Vec::with_capacity(D);
+    for i in 0..D {
+        working_set.push(base);
+        working_set[i][i] = 1.;
     }
-}
-
-impl<const DIM: usize> Ord for Point<DIM> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.partial_cmp(other).unwrap()
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct PointCloud<const DIM: usize> {
-    points: Vec<Point<DIM>>,
-}
-
-impl<const DIM: usize> PointCloud<DIM> {
-    fn new(points: Vec<Point<DIM>>) -> Self {
-        Self {
-            points: points.into_iter().sorted().dedup().collect(),
+    working_set.into_iter().permutations(D).map(|p| {
+        let mut m = na::SMatrix::<f64, D, D>::identity();
+        for i in 0..D {
+            for j in 0..D {
+                m[(i, j)] = p[i][j];
+            }
         }
-    }
+        m
+    })
+}
 
-    fn len(&self) -> usize {
-        self.points.len()
-    }
-
-    fn pairwise_distances(&self) -> Vec<f64> {
-        self.iter()
-            .cartesian_product(self)
-            .map(|(p1, p2)| {
-                (p1.pow(2) - p2.pow(2))
-                    .coords
-                    .iter()
-                    .map(|&x| x as f64)
-                    .sum::<f64>()
-                    .sqrt()
-            })
-            .collect()
-    }
-
-    fn iter(&self) -> impl Iterator<Item = &Point<DIM>> {
-        self.points.iter()
-    }
-
-    /// Matches if self.pairwise_differences() equals other.pairwise_differences().
-    /// This type of equality is rotation and translationn invariant.
-    fn matches(&self, other: &Self) -> bool {
-        if self.len() != other.len() {
+fn points_almost_equal<const D: usize>(p: &Point<D>, q: &Point<D>) -> bool {
+    for i in 0..D {
+        if (p[i] - q[i]).abs() > 1e-6 {
             return false;
         }
-        self.pairwise_distances() == other.pairwise_distances()
+    }
+    true
+}
+
+fn tup_to_opt<T>(opt1: Option<T>, opt2: Option<T>) -> Option<(T, T)> {
+    match (opt1, opt2) {
+        (Some(x), Some(y)) => Some((x, y)),
+        _ => None,
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct OPointCloud<const D: usize> {
+    points: Vec<Point<D>>,
+}
+
+impl<const D: usize> OPointCloud<D> {
+    fn new(points: Vec<Point<D>>) -> Self {
+        Self { points }
     }
 
-    fn matching_points(&self, other: &Self) -> Option<PointCloud<DIM>> {
-        let mut max_match = None;
-        for k in 2..self.len() {
+    fn from_ref_point_cloud<'a>(points: &'a RefPointCloud<D>) -> Self {
+        Self {
+            points: points.points.iter().map(|&p| p.clone()).collect(),
+        }
+    }
+
+    fn almost_equal(&self, other: &Self) -> bool {
+        self.points
+            .iter()
+            .all(|p| other.points.iter().any(|q| points_almost_equal(p, q)))
+    }
+
+    fn subsets(&self, size: usize) -> impl Iterator<Item = Vec<&Point<D>>> + '_ {
+        self.points.iter().combinations(size)
+    }
+
+    fn pairs(&self) -> impl Iterator<Item = (&Point<D>, &Point<D>)> + '_ {
+        self.subsets(2).map(|pair| (pair[0], pair[1]))
+    }
+
+    fn pairwise_distances(&'_ self) -> impl Iterator<Item = f64> + '_ {
+        self.pairs().map(|pair| (pair.0 - pair.1).abs().sum())
+    }
+
+    /// Two point clouds are isometric iff they have the same number of points and the same
+    /// pairwise distances. We need to use unordered collections to ignore permutations.
+    fn matches(&self, other: &Self) -> bool {
+        if !self.points.len() == other.points.len() {
+            return false;
+        }
+        let d1 = self
+            .pairwise_distances()
+            .sorted_by(|a, b| a.partial_cmp(b).unwrap());
+        let d2 = other
+            .pairwise_distances()
+            .sorted_by(|a, b| a.partial_cmp(b).unwrap());
+        d1.zip_eq(d2).all(|(x, y)| x == y)
+    }
+
+    fn maximal_matching_subsets<'a>(
+        &'a self,
+        other: &'a Self,
+    ) -> Option<(RefPointCloud<'_, D>, RefPointCloud<'_, D>)> {
+        let ref this = &RefPointCloud::from_point_cloud(self);
+        let other = &RefPointCloud::from_point_cloud(other);
+        let mut max_size = 0;
+        let mut max_subset_1 = None;
+        let mut max_subset_2 = None;
+        for k in 2..=this.points.len() {
             let mut found_match = false;
-            for s1 in self.subsets(k) {
+            for s1 in this.subsets(k) {
                 for s2 in other.subsets(k) {
                     if s1.matches(&s2) {
-                        max_match = Some(s1.clone());
                         found_match = true;
+                        if s1.points.len() > max_size {
+                            max_size = s1.points.len();
+                            max_subset_1 = Some(s1.clone());
+                            max_subset_2 = Some(s2.clone());
+                        }
                     }
                 }
             }
             if !found_match {
-                break;
+                return tup_to_opt(max_subset_1, max_subset_2);
             }
         }
-        max_match
+        tup_to_opt(max_subset_1, max_subset_2)
     }
 
-    fn subsets<'a>(&'a self, k: usize) -> impl Iterator<Item = PointCloud<DIM>> + 'a {
+    fn barycenter(&self) -> na::SVector<f64, D> {
+        let mut sum = na::SVector::from([0.; D]);
+        for p in &self.points {
+            sum += p.coords;
+        }
+        sum / self.points.len() as f64
+    }
+
+    fn translate(&self, offset: na::SVector<f64, D>) -> Self {
+        Self {
+            points: self.points.iter().map(|p| p + offset).collect(),
+        }
+    }
+
+    fn rotate(&self, mat: na::SMatrix<f64, D, D>) -> Self {
+        Self {
+            points: self.points.iter().map(|p| mat * p).collect(),
+        }
+    }
+
+    fn centered(&self) -> Self {
+        let barycenter = self.barycenter();
+        self.translate(-barycenter)
+    }
+
+    fn joined(&self, other: &Self) -> Self {
+        Self {
+            points: self
+                .points
+                .iter()
+                .chain(other.points.iter())
+                .cloned()
+                .sorted_by(cmp_points_lexical::<D>)
+                .dedup_by(points_almost_equal::<D>)
+                .collect(),
+        }
+    }
+
+    fn fuse(&self, other: &Self) -> Option<Self> {
+        let (s1, s2) = self.maximal_matching_subsets(other)?;
+        let b1 = s1.barycenter();
+        let b2 = s2.barycenter();
+
+        let c1 = s1.centered();
+        let c2 = s2.centered();
+
+        let mut pmat = None;
+        let mut rmat = None;
+        for permutation in permutation_matrices() {
+            for reflection in reflection_matrices() {
+                let rotated = c1.rotate(permutation * reflection);
+                if rotated.almost_equal(&c2) {
+                    pmat = Some(permutation);
+                    rmat = Some(reflection);
+                    break;
+                }
+            }
+        }
+        let pmat = pmat?;
+        let rmat = rmat?;
+
+        let pc1 = self.translate(-b1).rotate(pmat * rmat);
+        let pc2 = other.translate(-b2);
+
+        Some(pc1.joined(&pc2))
+    }
+}
+
+impl<const D: usize> FromStr for OPointCloud<D> {
+    type Err = Box<dyn Error>;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut points = Vec::new();
+        for line in s.lines() {
+            let mut iter = line.split(',');
+            let mut data = [0.; D];
+            for i in 0..D {
+                data[i] = iter
+                    .next()
+                    .ok_or("Parse Error: Not enough coordinates provided.".to_string())?
+                    .parse::<f64>()?;
+            }
+            points.push(Point::from(data));
+        }
+        Ok(Self::new(points))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct RefPointCloud<'a, const D: usize> {
+    points: Vec<&'a Point<D>>,
+}
+
+impl<'a, const D: usize> RefPointCloud<'a, D> {
+    fn new(points: Vec<&'a Point<D>>) -> Self {
+        Self { points }
+    }
+
+    fn from_point_cloud(cloud: &'a OPointCloud<D>) -> Self {
+        Self::new(cloud.points.iter().collect())
+    }
+
+    fn subsets(&self, size: usize) -> impl Iterator<Item = RefPointCloud<'a, D>> + '_ {
         self.points
             .iter()
-            .combinations(k)
-            .map(|points| -> PointCloud<DIM> {
-                let mut result = Self::new(points.into_iter().cloned().collect());
-                result
-            })
+            .combinations(size)
+            .map(|subset| Self::new(subset.into_iter().map(|&p| p).collect()))
+    }
+
+    fn pairs(&'a self) -> impl Iterator<Item = (&'a Point<D>, &'a Point<D>)> + '_ {
+        self.subsets(2).map(|pair| (pair.points[0], pair.points[1]))
+    }
+
+    fn pairwise_distances(&'a self) -> impl Iterator<Item = f64> + '_ {
+        self.pairs().map(|pair| (pair.0 - pair.1).abs().sum())
+    }
+
+    /// Two point clouds are isometric iff they have the same number of points and the same
+    /// pairwise distances. We need to use unordered collections to ignore permutations.
+    fn matches(&self, other: &Self) -> bool {
+        if !self.points.len() == other.points.len() {
+            return false;
+        }
+        let d1 = self
+            .pairwise_distances()
+            .sorted_by(|a, b| a.partial_cmp(b).unwrap());
+        let d2 = other
+            .pairwise_distances()
+            .sorted_by(|a, b| a.partial_cmp(b).unwrap());
+        d1.zip_eq(d2).all(|(x, y)| x == y)
+    }
+
+    fn maximal_matching_subsets(
+        &'a self,
+        other: &'a Self,
+    ) -> Option<(RefPointCloud<'_, D>, RefPointCloud<'_, D>)> {
+        let mut max_size = 0;
+        let mut max_subset_1 = None;
+        let mut max_subset_2 = None;
+        for k in 2..=self.points.len() {
+            let mut found_match = false;
+            for s1 in self.subsets(k) {
+                for s2 in other.subsets(k) {
+                    if s1.matches(&s2) {
+                        found_match = true;
+                        if s1.points.len() > max_size {
+                            max_size = s1.points.len();
+                            max_subset_1 = Some(s1.clone());
+                            max_subset_2 = Some(s2.clone());
+                        }
+                    }
+                }
+            }
+            if !found_match {
+                return tup_to_opt(max_subset_1, max_subset_2);
+            }
+        }
+        tup_to_opt(max_subset_1, max_subset_2)
+    }
+
+    fn barycenter(&self) -> na::SVector<f64, D> {
+        let mut sum = na::SVector::<f64, D>::from([0.; D]);
+        for &p in self.points.iter() {
+            sum += na::SVector::<f64, D>::from(p.coords);
+        }
+        sum / self.points.len() as f64
+    }
+
+    fn translate(&self, offset: na::SVector<f64, D>) -> OPointCloud<D> {
+        OPointCloud {
+            points: self.points.iter().map(|&p| p + offset).collect(),
+        }
+    }
+
+    fn rotate(&self, mat: na::SMatrix<f64, D, D>) -> OPointCloud<D> {
+        OPointCloud {
+            points: self.points.iter().map(|&p| mat * p).collect(),
+        }
+    }
+
+    fn centered(&self) -> OPointCloud<D> {
+        let barycenter = self.barycenter();
+        self.translate(-barycenter)
     }
 }
-
-impl<const DIM: usize> IntoIterator for PointCloud<DIM> {
-    type Item = Point<DIM>;
-    type IntoIter = std::vec::IntoIter<Point<DIM>>;
-    fn into_iter(self) -> Self::IntoIter {
-        self.points.into_iter()
-    }
-}
-
-impl<'a, const DIM: usize> IntoIterator for &'a PointCloud<DIM> {
-    type Item = &'a Point<DIM>;
-    type IntoIter = std::slice::Iter<'a, Point<DIM>>;
-    fn into_iter(self) -> Self::IntoIter {
-        self.points.iter()
-    }
-}
-
-impl<const DIM: usize> FromIterator<Point<DIM>> for PointCloud<DIM> {
-    fn from_iter<I: IntoIterator<Item = Point<DIM>>>(iter: I) -> Self {
-        let mut result = Self::new(iter.into_iter().collect());
-        result
-    }
-}
-
-struct Scanner {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_point_cloud() {
-        let mut cloud = PointCloud::new(vec![
-            Point::new([0, 0]),
-            Point::new([1, 0]),
-            Point::new([0, 1]),
-            Point::new([1, 1]),
-            Point::new([2, 0]),
-            Point::new([0, 2]),
-            Point::new([2, 1]),
-            Point::new([1, 2]),
-            Point::new([2, 2]),
-        ]);
-        assert_eq!(cloud.len(), 9);
+    fn cmp_points() {
+        let p1 = Point::from([1., 2., 3.]);
+        let p2 = Point::from([1., 2., 3.]);
+        let p3 = Point::from([1., 2., 4.]);
+        let p4 = Point::from([-2., 2., 3.]);
 
-        let mut dists = cloud.pairwise_distances();
-        assert_eq!(dists.len(), 9);
+        assert_eq!(cmp_points_lexical(&p1, &p2), Ordering::Equal);
+        assert_eq!(cmp_points_lexical(&p1, &p3), Ordering::Less);
 
-        for p in cloud.iter() {
-            assert!(p.coords[0].abs() + p.coords[1].abs() <= 4);
+        let points = vec![p1, p2, p3, p4];
+        let sorted: Vec<Point<3>> = points
+            .into_iter()
+            .sorted_by(|a, b| cmp_points_lexical(a, b))
+            .collect();
+
+        assert_eq!(sorted, vec![p4, p1, p2, p3]);
+    }
+
+    #[test]
+    fn test_mirror_matrices() {
+        for mat in reflection_matrices::<2>() {
+            println!("{}", mat);
         }
     }
 
     #[test]
-    fn test_matches() {
-        todo!()
+    fn test_permutation_matrices() {
+        for mat in permutation_matrices::<2>() {
+            println!("{}", mat);
+        }
+    }
+
+    #[test]
+    fn test_point_cloud_2() {
+        let input = "1,1\n1,6\n8,3\n3,4\n5,5\n8,9";
+        let cloud = OPointCloud::<2>::from_str(input).unwrap();
+        assert_eq!(cloud.points.len(), 6);
+        assert_eq!(cloud.points[0], Point::from_slice(&[1., 1.]));
+    }
+
+    #[test]
+    fn test_point_cloud_3() {
+        let input = "1,1,1\n1,6,1\n8,3,1\n3,4,1\n5,5,1\n8,9,1";
+        let cloud = OPointCloud::<3>::from_str(input).unwrap();
+        assert_eq!(cloud.points.len(), 6);
+        assert_eq!(cloud.points[0], Point::from_slice(&[1., 1., 1.]));
+        assert_eq!(cloud.points[1], Point::from_slice(&[1., 6., 1.]));
+        assert_eq!(cloud.points[2], Point::from_slice(&[8., 3., 1.]));
+    }
+
+    #[test]
+    fn test_point_cloud_3_distances() {
+        let input = "1,1,1\n1,6,1\n8,3,1\n3,4,1\n5,5,1\n8,9,1";
+        let cloud = OPointCloud::<3>::from_str(input).unwrap();
+        let distances: Vec<f64> = cloud.pairwise_distances().collect();
+        assert_eq!(distances.len(), 15);
+    }
+
+    #[test]
+    fn test_point_cloud_3_matches() {
+        let input = "1,1,1\n1,6,1\n8,3,1\n3,4,1\n5,5,1\n8,9,1";
+        let cloud = OPointCloud::<3>::from_str(input).unwrap();
+
+        let input = "1,6,1\n1,1,1\n8,3,1\n3,4,1\n5,5,1\n8,9,1";
+        let other = OPointCloud::<3>::from_str(input).unwrap();
+        assert!(cloud.matches(&other));
+    }
+
+    #[test]
+    fn test_point_cloud_3_maximal_matching_subset() {
+        let input = "1,1,1\n1,6,1\n8,3,1\n3,4,1\n5,5,1\n8,9,1";
+        let cloud = OPointCloud::<3>::from_str(input).unwrap();
+
+        let input = "2,7,2\n2,2,2\n9,4,2\n1,2,3\n4,5,6\n7,8,9";
+        let other = OPointCloud::<3>::from_str(input).unwrap();
+        let (s1, s2) = cloud.maximal_matching_subsets(&other).unwrap();
+
+        assert_eq!(s1.points.len(), 3);
+        assert_eq!(s2.points.len(), 3);
+    }
+
+    #[test]
+    fn test_centered() {
+        let input = "1,1,1\n1,6,1\n8,3,1\n3,4,1\n5,5,1\n8,9,1";
+        let cloud = OPointCloud::<3>::from_str(input).unwrap();
+
+        let input = "2,2,2\n2,7,2\n9,4,2\n1,2,3\n4,5,6\n7,8,9";
+        let other = OPointCloud::<3>::from_str(input).unwrap();
+        let (s1, s2) = cloud.maximal_matching_subsets(&other).unwrap();
+
+        let s1 = s1.centered();
+        let s2 = s2.centered();
+
+        assert!(s1
+            .points
+            .iter()
+            .zip_eq(s2.points.iter())
+            .all(|(p1, p2)| (p1 - p2).norm() < 1e-6));
+    }
+
+    #[test]
+    fn test_fuse_translated() {
+        let input = "1,1,1\n1,6,1\n8,3,1\n3,4,1\n5,5,1\n8,9,1";
+        let cloud_1 = OPointCloud::<3>::from_str(input).unwrap();
+
+        let input = "2,2,2\n2,7,2\n9,4,2\n1,2,3\n4,5,6\n7,8,9";
+        let cloud_2 = OPointCloud::<3>::from_str(input).unwrap();
+
+        let fused = cloud_1.fuse(&cloud_2).unwrap();
+
+        assert_eq!(fused.points.len(), 9);
+    }
+
+    #[test]
+    fn test_fuse_transformed() {
+        let input = "1,1\n1,6\n8,3\n3,4\n5,5\n8,9";
+        let cloud_1 = OPointCloud::<2>::from_str(input).unwrap();
+
+        let input = "-1,-1\n-6,-1\n-3,-8\n1,2\n3,4\n5,6";
+        let cloud_2 = OPointCloud::<2>::from_str(input).unwrap();
+
+        let fused = cloud_1.fuse(&cloud_2).unwrap();
+
+        assert_eq!(fused.points.len(), 9);
     }
 }
